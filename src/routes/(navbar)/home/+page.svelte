92bd18bf0880eca939f8c0bb647911996f1b542f
Siam11651 <siam11651@outlook.com>
2024-01-28 05:24:56 +0600

    kaaj kori

--- src/routes/(navbar)/home/+page.svelte
+++ src/routes/(navbar)/home/+page.svelte
@@ -80,14 +80,7 @@
 
   let personal_files: File[] = new Array(0);
   $: personal_files_length = personal_files.length;
-
-  for (let i: number = 0; i < personal_files.length; ++i) {
-    personal_files[i] = new File();
-    personal_files[i].name = "Personal File " + (i + 1).toString();
-    personal_files[i].type = ["png", "pdf", "jpg"][
-      Math.round(Math.random() * 2)
-    ];
-  }
+  let personal_files_loading: boolean = true;
 
   /**
    * team list element data object
@@ -122,6 +115,7 @@
   }
 
   function get_personal_files(): void {
+    personal_files_loading = true;
     let request_obj: any = {
       given_userid: $page.data.session?.user?.name,
     };
@@ -144,6 +138,8 @@
           personal_files[i].name = response_obj[i].f_filename;
           personal_files[i].type = response_obj[i].f_file_extension;
         }
+
+        personal_files_loading = false;
       }
     );
   }
@@ -261,6 +257,8 @@
   };
 
   onMount((): void => {
+    personal_files_loading = true;
+
     if ($page.data.session === null) {
       goto("/");
     } else {
@@ -434,22 +432,34 @@
         >
           My Personal Files
         </p>
-        {#if personal_files_length > 0}
-          <ul class="list-elements space-y-2 pb-1 pe-1" style="overflow-y: auto;">
-            {#each personal_files as file}
-              <li>
-                <FileCard
-                  file_id={file.id}
-                  file_name={file.name}
-                  file_type={file.type}
-                />
-              </li>
-            {/each}
-          </ul>
-        {:else}
-          <div class="list-empty flex justify-center p-4 text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300" role="alert">
-            <p>Wow! Such empty *_*</p>
+        {#if personal_files_loading}
+          <div class="list-elements flex items-center justify-center">
+            <div role="status">
+                <svg aria-hidden="true" class="w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                </svg>
+                <span class="sr-only">Loading...</span>
+            </div>
           </div>
+        {:else}
+          {#if personal_files_length > 0}
+            <ul class="list-elements space-y-2 pb-1 pe-1" style="overflow-y: auto;">
+              {#each personal_files as file}
+                <li>
+                  <FileCard
+                    file_id={file.id}
+                    file_name={file.name}
+                    file_type={file.type}
+                  />
+                </li>
+              {/each}
+            </ul>
+          {:else}
+            <div class="list-empty flex justify-center p-4 text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300" role="alert">
+              <p>Wow! Such empty *_*</p>
+            </div>
+          {/if}
         {/if}
         <div class="list-upload flex justify-end">
           <div class="flex flex-col justify-end">
