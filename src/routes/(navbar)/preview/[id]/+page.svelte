d276e14b922c49d47155310b5e66aa1ca8b3adf0
Siam11651 <siam11651@outlook.com>
2024-01-28 04:55:07 +0600

    kaaj kori

--- src/routes/(navbar)/preview/[id]/+page.svelte
+++ src/routes/(navbar)/preview/[id]/+page.svelte
@@ -121,6 +121,18 @@
             <img class="img-preview rounded" src={file_view_link} alt="img-placeholder" />
         {:else if file_type === 2}
             <embed class="pdf-preview rounded" src={file_view_link} />
+        {:else}
+            <div class="flex flex-col justify-center" style="height: 100%;">
+                <div class="flex items-center justify-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                    <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                    </svg>
+                    <span class="sr-only">Info</span>
+                    <div>
+                        <p>No Preview Available T_T</p>
+                    </div>
+                </div>
+            </div>
         {/if}
     </div>
     <div class="flex flex-col justify-between block p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
