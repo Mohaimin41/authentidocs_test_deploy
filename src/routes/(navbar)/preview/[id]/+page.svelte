35bd9e30e601873bac53e7d87af3dfae305736d1
Siam11651 <siam11651@outlook.com>
2024-01-28 02:03:45 +0600

    kaaj kori

--- src/routes/(navbar)/preview/[id]/+page.svelte
+++ src/routes/(navbar)/preview/[id]/+page.svelte
@@ -1,32 +1,54 @@
 <script lang="ts">
     import { page } from "$app/stores";
     import { common_fetch } from "$lib/fetch_func";
+    import { Modal } from "flowbite";
     import { onMount } from "svelte";
 
     let file_name: string = "";
     let file_type: number = 0;
     let download_anchor: HTMLAnchorElement;
+    let file_view_link: string;
+    let file_download_link: string;
+    let modal_elem: HTMLDivElement;
+    let modal_show: boolean = false;
+    let modal_obj: Modal;
 
-    let file_view_link: string ="";
-    let file_download_link: string="";
+    function hide_modal(): void
+    {
+        modal_obj.hide();
+    }
+
+    function show_modal(): void
+    {
+        modal_obj.show();
+    }
 
     onMount(async (): Promise<void> =>
     {
+        modal_obj = new Modal(modal_elem,
+        {
+            onHide: (): void =>
+            {
+                modal_show = false;
+            },
+            onShow: (): void =>
+            {
+                modal_show = false;
+            }
+        });
+
         let request_obj: any =
         {
             fileid: $page.params.id,
             user_id: $page.data.session?.user?.name
         };
+
         common_fetch("/api/files/getfilelink", request_obj,
         async (response: Response): Promise<void> =>
         {
             let response_obj: any = await response.json();
-            console.log(response_obj);
-             file_name = response_obj.file_data.filename;
-             file_type = response_obj.file_data.file_mimetype;
-
-             console.log(file_name);
-
+            file_name = response_obj.file_data.filename;
+            file_type = response_obj.file_data.file_mimetype;
             let mime_text: string = "Application/octet-stream";
 
             if(file_type === 1)
@@ -40,16 +62,13 @@
             
              file_view_link =response_obj.file_link_preview;
              file_download_link = response_obj.file_link_download;
-             console.log(file_view_link)
-             console.log(file_download_link)
              download_anchor.download = file_download_link;
         });
-
     });
 </script>
 
-<div class="preview-root">
-    <div class="preview-body block p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2">
+<div class="preview-root flex flex-col">
+    <div class="preview-body flex-grow block p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2">
         {#if file_type === 0}
             <div></div>
         {:else if file_type === 1}
@@ -107,39 +126,82 @@
                 </div>
             </div>
         </div>
-        <div class="flex justify-end">
+        <div class="flex justify-end mt-3">
             <!-- certificate button -->
-            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">View Certificate</button>
+            <button on:click={show_modal} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">View Certificate</button>
             <!-- download button -->
             <a download={file_name} href={file_download_link} bind:this={download_anchor} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ms-2">Download</a>
         </div>
     </div>
 </div>
 
+<div
+  bind:this={modal_elem}
+  tabindex="-1"
+  aria-hidden="true"
+  class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+>
+  <div class="relative p-4 w-full max-w-2xl max-h-full">
+    <!-- Modal content -->
+    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+      <!-- Modal header -->
+      <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+          File Certificate
+        </h3>
+        <button on:click={hide_modal} type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="static-modal">
+          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+          </svg>
+          <span class="sr-only">Close modal</span>
+        </button>
+      </div>
+      <!-- Modal body -->
+      <div class="p-4 md:p-5 space-y-4">
+        <div class="w-full">
+          <p class="text-base font-semibold text-gray-500 dark:text-gray-400">Signed By</p>
+          <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">123</p>
+          <p class="text-base font-semibold text-gray-500 dark:text-gray-400">Signed On</p>
+          <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">123</p>
+          <p class="text-base font-semibold text-gray-500 dark:text-gray-400">Signature</p>
+          <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">123</p>
+          <p class="text-base font-semibold text-gray-500 dark:text-gray-400">Public Key</p>
+          <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">123</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+{#if modal_show}
+  <div class="bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40" />
+{/if}
+
 <style>
     .preview-root
     {
         position: absolute;
-        top: 12vh;
-        bottom: 2vh;
+        top: 5.25rem;
+        bottom: 1rem;
         left: 20%;
         right: 20%;
     }
     .preview-body
     {
-        position: absolute;
+        max-height: 70%;
+        /* position: absolute;
         top: 0;
         bottom: 30%;
         left: 0;
-        right: 0;
+        right: 0; */
     }
     .preview-meta
     {
-        position: absolute;
+        /* position: absolute;
         top: 70%;
         bottom: 0;
         left: 0;
-        right: 0;
+        right: 0; */
     }
     .img-preview
     {
