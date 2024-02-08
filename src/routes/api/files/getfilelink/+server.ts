66a6993e8b45bf6ebcbbfb48774d7fed03aff5b1
Sadif Ahmed <1905058@ugrad.cse.buet.ac.bd>
2024-02-08 19:36:47 +0600

    ...

--- src/routes/api/files/getfilelink/+server.ts
+++ src/routes/api/files/getfilelink/+server.ts
@@ -27,7 +27,10 @@ export async function POST({
     }
   );
   if (_error1) {
-    console.log("ERROR @api/files/getfilelink:30: supabase getting file data error\n", _error1)
+    console.log(
+      "ERROR @api/files/getfilelink:30: supabase getting file data error\n",
+      _error1
+    );
     return new Response(
       JSON.stringify(
         "internal server error while getting file metadata: " + _error1
@@ -40,51 +43,103 @@ export async function POST({
       }
     );
   }
-  const { data: result2, error: _error2 } = await supabase.storage
-    .from("user_personal_files")
-    .createSignedUrl(result1.file_url, 24 * 60 * 60);
+  let result4, result5;
+  if (result1.current_state == "personal") {
+    const { data: result2, error: _error2 } = await supabase.storage
+      .from("user_personal_files")
+      .createSignedUrl(result1.file_url, 24 * 60 * 60);
 
-  if (_error2) {
-    
-    console.log("ERROR @api/files/getfilelink:49: supabase file link access error\n", _error2)
-    return new Response(
-      JSON.stringify(
-        "internal server error while getting preview link: " + _error2
-      ),
+    if (_error2) {
+      console.log(
+        "ERROR @api/files/getfilelink:49: supabase file link access error\n",
+        _error2
+      );
+      return new Response(
+        JSON.stringify(
+          "internal server error while getting preview link: " + _error2
+        ),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          status: 500,
+        }
+      );
+    }
+    result4 = result2
+    const { data: result3, error: _error3 } = await supabase.storage
+      .from("user_personal_files")
+      .createSignedUrl(result1.file_url, 24 * 60 * 60, {
+        download: result1.filename,
+      });
+
+    if (_error3) {
+      console.log(
+        "ERROR @api/files/getfilelink:70: supabase file link access error\n",
+        _error3
+      );
+      return new Response(
+        JSON.stringify(
+          "internal server error while getting downloadable link: " + _error3
+        ),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          status: 500,
+        }
+      );
+    }
+    result5 = result3
+  } else {
+    let { data: result1, error: _error1 } = await supabase.rpc(
+      "get_single_filemetadata_fileid",
       {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        status: 500,
+        given_fileid,
+        given_userid,
       }
     );
-  }
-
-  const { data: result3, error: _error3 } = await supabase.storage
-    .from("user_personal_files")
-    .createSignedUrl(result1.file_url, 24 * 60 * 60, {
-      download: result1.filename,
-    });
-
-  if (_error3) {
-    console.log("ERROR @api/files/getfilelink:70: supabase file link access error\n", _error3)
-    return new Response(
-      JSON.stringify(
-        "internal server error while getting downloadable link: " + _error3
-      ),
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        status: 500,
-      }
-    );
+    if (_error1) {
+      console.log("ERROR @api/files/getfilelink:30: supabase getting file data error\n", _error1)
+      return new Response(
+        JSON.stringify(
+          "internal server error while getting file metadata: " + _error1
+        ),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          status: 500,
+        }
+      );
+    }
+    result4 = result1
+    const { data: result2, error: _error2 } = await supabase.storage
+      .from("files")
+      .createSignedUrl(result1.file_url, 24 * 60 * 60);
+  
+    if (_error2) {
+      
+      console.log("ERROR @api/files/getfilelink:49: supabase file link access error\n", _error2)
+      return new Response(
+        JSON.stringify(
+          "internal server error while getting preview link: " + _error2
+        ),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          status: 500,
+        }
+      );
+    }
+    result5 = result2
   }
 
   let response: Response = new Response(
     JSON.stringify({
-      file_link_preview: result2?.signedUrl,
-      file_link_download: result3?.signedUrl,
+      file_link_preview: result4?.signedUrl,
+      file_link_download: result5?.signedUrl,
       file_data: result1,
     }),
     {
