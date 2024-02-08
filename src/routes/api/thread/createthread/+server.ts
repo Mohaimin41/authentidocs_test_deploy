b33918ff92e33f69e554ba9e85eaf784c11e81a7
Sadif Ahmed <1905058@ugrad.cse.buet.ac.bd>
2024-02-08 13:46:05 +0600

    thread apis

---
+++ src/routes/api/thread/createthread/+server.ts
@@ -0,0 +1,51 @@
+import { supabase } from "$lib/server/supabase_client.server";
+import type { RequestEvent } from "./$types";
+
+export async function POST({
+  request,
+  cookies,
+  locals,
+}: RequestEvent): Promise<Response> {
+  const session = await locals.getSession();
+  if (!session?.user) {
+    return new Response(JSON.stringify("you must be logged in to add files"), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      status: 401,
+    });
+  }
+  // console.log(session);
+  const thread_info = await request.json();
+  // console.log("inside add key",key_info);
+  let given_description = thread_info.description;
+  let given_parent_teamid = thread_info.given_parent_teamid;
+  let given_threadname =  thread_info.given_threadname;
+
+
+  let { data:result , error: _error } = await supabase
+  .rpc('create_thread', {
+    given_description, 
+    given_parent_teamid, 
+    given_threadname
+  })
+
+  // console.log("add key rps result",result)
+  if (_error) {
+    console.log("ERROR @api/user/addkey:33: supabase add user publickey error\n", _error)
+    return new Response(JSON.stringify("internal server error while adding user key: " + _error), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      status: 500,
+    });
+  }
+
+  let response: Response = new Response(JSON.stringify(result), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return response;
+}
