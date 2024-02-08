9ffaa94c9280bf97d68aaffa3ebefa5b01f7db1f
Siam11651 <siam11651@outlook.com>
2024-02-08 16:18:04 +0600

    kaaj kori

--- src/routes/(navbar)/team/[id]/+page.svelte
+++ src/routes/(navbar)/team/[id]/+page.svelte
@@ -1,10 +1,48 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import ThreadCard from "$lib/components/team/thread-card.svelte";
+    import { Modal, initModals } from "flowbite";
+    import { page } from "$app/stores";
     
+    let id: string;
+    let thread_name_input: string;
+    let thread_description_input: string;
+
+    function create_thread(): void
+    {
+        let temp_desc: string | undefined = thread_description_input;
+
+        if(temp_desc === undefined)
+        {
+            temp_desc = "";
+        }
+
+        fetch("/api/thread/createthread",
+        {
+            method: "POST",
+            headers:
+            {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(
+            {
+                given_parent_teamid: id,
+                given_threadname: thread_name_input,
+                description: temp_desc
+            })
+        }).then(async (response: Response): Promise<void> =>
+        {
+            let response_obj: any = await response.json();
+
+            console.log(response_obj);
+        });
+    }
+
     onMount((): void =>
     {
-        
+        initModals();
+
+        id = $page.params.id;
     });
 </script>
 
@@ -107,7 +145,41 @@
     </ul>
 </div>
 <div class="thread-button flex justify-end items-end">
-    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create Thread</button>
+    <button data-modal-target="create-thread-modal" data-modal-toggle="create-thread-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create Thread</button>
+</div>
+
+<div id="create-thread-modal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-2xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Create Thread
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="create-thread-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div class="p-4 md:p-5 space-y-4">
+                <form on:submit={create_thread} class="mx-auto">
+                    <div class="mb-5">
+                      <label for="create-thread-name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Thread Name</label>
+                      <input bind:value={thread_name_input} type="text" id="create-thread-name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" autocomplete="off" required>
+                    </div>
+                    <label for="create-thread-description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Thread Description</label>
+                    <textarea bind:value={thread_description_input} id="create-thread-description" rows="3" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" autocomplete="off"></textarea>
+                    <div class="flex justify-end">
+                        <button type="submit" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Confirm</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 
 <style>
