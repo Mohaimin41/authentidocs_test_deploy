d6049f633ca490779d66ee5b1fb96bf87d02c037
Sadif Ahmed <1905058@ugrad.cse.buet.ac.bd>
2024-02-08 19:54:37 +0600

    ...

--- src/routes/api/files/getfilelink/+server.ts
+++ src/routes/api/files/getfilelink/+server.ts
@@ -69,7 +69,7 @@ export async function POST({
         }
       );
     }
-    result4 = result2
+    result4 = result2;
     const { data: result3, error: _error3 } = await supabase.storage
       .from("user_personal_files")
       .createSignedUrl(result1.file_url, 24 * 60 * 60, {
@@ -93,37 +93,19 @@ export async function POST({
         }
       );
     }
-    result5 = result3
-  } else {
-    let { data: result1, error: _error1 } = await supabase.rpc(
-      "get_single_filemetadata_fileid",
-      {
-        given_fileid,
-        given_userid,
-      }
-    );
-    if (_error1) {
-      console.log("ERROR @api/files/getfilelink:30: supabase getting file data error\n", _error1)
-      return new Response(
-        JSON.stringify(
-          "internal server error while getting file metadata: " + _error1
-        ),
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-          status: 500,
-        }
-      );
-    }
-    result4 = result1
+    result5 = result3;
+  } 
+  else {
+
     const { data: result2, error: _error2 } = await supabase.storage
       .from("files")
       .createSignedUrl(result1.file_url, 24 * 60 * 60);
-  
+
     if (_error2) {
-      
-      console.log("ERROR @api/files/getfilelink:49: supabase file link access error\n", _error2)
+      console.log(
+        "ERROR @api/files/getfilelink:49: supabase file link access error\n",
+        _error2
+      );
       return new Response(
         JSON.stringify(
           "internal server error while getting preview link: " + _error2
@@ -136,7 +118,31 @@ export async function POST({
         }
       );
     }
-    result5 = result2
+    result4 = result2;
+    const { data: result3, error: _error3 } = await supabase.storage
+      .from("files")
+      .createSignedUrl(result1.file_url, 24 * 60 * 60, {
+        download: result1.filename,
+      });
+
+    if (_error3) {
+      console.log(
+        "ERROR @api/files/getfilelink:70: supabase file link access error\n",
+        _error3
+      );
+      return new Response(
+        JSON.stringify(
+          "internal server error while getting downloadable link: " + _error3
+        ),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          status: 500,
+        }
+      );
+    }
+    result5 = result3;
   }
 
   let response: Response = new Response(
