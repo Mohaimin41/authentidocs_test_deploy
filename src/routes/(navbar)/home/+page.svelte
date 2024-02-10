f2381e88be0ed287bb274aaece706ac59e5d5b1d
Siam11651 <siam11651@outlook.com>
2024-02-10 23:06:23 +0600

    kaaj kori

--- src/routes/(navbar)/home/+page.svelte
+++ src/routes/(navbar)/home/+page.svelte
@@ -199,6 +199,8 @@
       upload_progress_elem.style.width = Math.round(i * 100 / file_buffer.byteLength) + "%";
     }
     if (success) {
+      upload_progress_elem.style.width = "100%";
+
       await fetch("/api/files/addchunkfile/finish?filename=" + file.name, {
         method: "POST",
         headers: {
@@ -535,31 +537,37 @@
         class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
       >
         <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-          File Upload
+          {#if uploading}
+            Uploading...
+          {:else}
+            File Upload
+          {/if}
         </h3>
-        <button
-          on:click={hide_modal}
-          type="button"
-          class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-          data-modal-hide="static-modal"
-        >
-          <svg
-            class="w-3 h-3"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 14"
+        {#if !uploading}
+          <button
+            on:click={hide_modal}
+            type="button"
+            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            data-modal-hide="static-modal"
           >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-            />
-          </svg>
-          <span class="sr-only">Close modal</span>
-        </button>
+            <svg
+              class="w-3 h-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
+            </svg>
+            <span class="sr-only">Close modal</span>
+          </button>
+        {/if}
       </div>
       <!-- Modal body -->
       <div class="p-4 md:p-5 space-y-4">
