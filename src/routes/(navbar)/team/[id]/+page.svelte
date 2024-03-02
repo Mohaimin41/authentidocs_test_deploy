8e6fa8b9d66e150b8e887534cb71ec1d61ee9dba
Sadif Ahmed <1905058@ugrad.cse.buet.ac.bd>
2024-03-02 18:41:12 +0600

    ...

--- src/routes/(navbar)/team/[id]/+page.svelte
+++ src/routes/(navbar)/team/[id]/+page.svelte
@@ -422,6 +422,25 @@
 
     //console.log(response_obj);
   }
+  let is_member:boolean = false;
+  async function check_member(): Promise<void> {
+    let response: Response = await fetch(
+    "/api/user/ismember",
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            level:'team',
+            level_id:id,
+            id:$page.data.session?.user?.name,
+        })
+    });
+    let response_obj: any = await response.json();
+    console.log(response_obj)
+    is_member=response_obj;   
+  }
   async function check_admin(): Promise<void> {
     let response: Response = await fetch("/api/user/isadmin", {
       method: "POST",
@@ -565,6 +584,7 @@
     get_members();
     get_files();
     check_admin();
+    check_member();
     }
 
   });
@@ -674,7 +694,7 @@
                   {/each}
               </List>
               <div class="flex justify-end mt-2">
-                  <button on:click={() => {create_thread_modal.show();}} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mx-2 mb-2">Create Thread</button>
+                  <button on:click={() => {create_thread_modal.show();}} type="button" disabled={!is_member} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mx-2 mb-2">Create Thread</button>
               </div>
           {:else if tabs[2].active}
               <div class="mb-2">
@@ -701,7 +721,7 @@
               </List>
                <!-- Add File -->
               <div class="flex justify-end mt-2">
-              <button on:click={add_file} type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" >Add File</button>
+              <button on:click={add_file} type="button" disabled={!is_member} class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" >Add File</button>
             </div>
               {:else if tabs[3].active}
             <div class="mb-2">
@@ -727,7 +747,7 @@
                 {/each}
             </List>
             <div class="flex justify-end mt-2">
-                <button on:click={() => {add_member_modal.show();}} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ms-2 mb-2">Add Member</button>
+                <button on:click={() => {add_member_modal.show();}} type="button" disabled={!is_member} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ms-2 mb-2">Add Member</button>
             </div>
           {:else if tabs[4].active}
             <div class="mb-2">
@@ -753,7 +773,7 @@
                 {/each}
             </List>
             <div class="flex justify-end mt-2">
-                <button on:click={() => {send_notice_modal.show();}} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mx-2 mb-2">Send Notice</button>
+                <button on:click={() => {send_notice_modal.show();}} type="button" disabled={!is_member} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mx-2 mb-2">Send Notice</button>
             </div>
         {/if}
     </div>
