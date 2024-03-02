3556b51cb7ecee8c3aa84a9675a64fe41409d0e5
Siam11651 <siam11651@outlook.com>
2024-03-02 12:29:23 +0600

    kaaj kori

--- src/routes/(navbar)/dashboard/+page.svelte
+++ src/routes/(navbar)/dashboard/+page.svelte
@@ -96,6 +96,7 @@
   let cng_pass_new: string;
   let cng_pass_confirm: string;
   let cng_pass_confirm_elem: HTMLInputElement;
+  let invalid_pass: boolean = false;
 
   async function cng_pass(): Promise<void>
   {
@@ -107,6 +108,37 @@
     }
 
     let password_hash: string = await make_hash(cng_pass_new);
+    let old_hash: string = await make_hash(cng_pass_old);
+
+    fetch("/api/user/editpwd",
+    {
+      method: "POST",
+      body: JSON.stringify(
+      {
+        userid: $page.data.session?.user?.name,
+        new_pwd_hash: password_hash,
+        old_pwd_hash: old_hash
+      })
+    }).then(async (response: Response): Promise<void> =>
+    {
+      if(response.status === 200)
+      {
+        let response_obj: any = await response.json();
+
+        if(response_obj === true)
+        {
+          cng_pass_modal.hide();
+        }
+        else
+        {
+          invalid_pass = true;
+        }
+      }
+      else
+      {
+        console.error(response.status, response.statusText);
+      }
+    });
   }
 
   /**
@@ -211,7 +243,7 @@
         if (response_obj === null) {
           return;
         }
-        console.log(response_obj);
+        // console.log(response_obj);
 
         for (let i: number = 0; i < response_obj.length; ++i) {
           work_files[i] = new File();
@@ -290,7 +322,7 @@
         if (response_obj === null) {
           return;
         }
-        console.log(response_obj);
+        // console.log(response_obj);
         for (let i: number = 0; i < response_obj.length; ++i) {
           threads[i] = new Thread();
           threads[i].name = response_obj[i].f_threadname;
@@ -313,7 +345,7 @@
         if (response_obj === null) {
           return;
         }
-        console.log(response_obj);
+        // console.log(response_obj);
         for (let i: number = 0; i < response_obj.length; ++i) {
           orgs[i] = new Entity();
           orgs[i].name = response_obj[i].f_org_name;
@@ -325,7 +357,19 @@
 
 
   onMount((): void => {
-    cng_pass_modal = new Modal(cng_pass_modal_elem);
+    cng_pass_modal = new Modal(cng_pass_modal_elem,
+    {
+      onShow: (): void =>
+      {
+        invalid_pass = false;
+      },
+      onHide: (): void =>
+      {
+        cng_pass_old = "";
+        cng_pass_new = "";
+        cng_pass_confirm = "";
+      }
+    });
 
     if ($page.data.session === null) {
       goto("/");
@@ -735,17 +779,20 @@
               <div class="mb-5">
                 <label for="cng-pass-old" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current Password</label>
                 <input bind:value={cng_pass_old} type="password" id="cng-pass-old" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                {#if invalid_pass}
+                  <p class="mt-2 text-sm text-red-600 dark:text-red-500">Invalid password</p>
+                {/if}
               </div>
               <div class="mb-5">
                 <label for="cng-pass-new" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
-                <input bind:value={cng_pass_new} type="password" id="cng-pass-old" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                <input bind:value={cng_pass_new} type="password" id="cng-pass-new" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
               </div>
               <div class="mb-5">
                 <label for="cng-pass-confirm" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
-                <input bind:this={cng_pass_confirm_elem} bind:value={cng_pass_confirm} type="password" id="cng-pass-old" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                <input bind:this={cng_pass_confirm_elem} bind:value={cng_pass_confirm} type="password" id="cng-pass-confirm" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
               </div>
               <div class="flex justify-end">
-                <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Confirm</button>
+                <button type="submit" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Confirm</button>
               </div>
             </form>
         </div>
