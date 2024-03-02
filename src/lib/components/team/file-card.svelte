33de0eef0bc55ecdf26e8f660efa8a8251954726
Siam11651 <siam11651@outlook.com>
2024-03-03 05:46:16 +0600

    kaaj kori

--- src/lib/components/team/file-card.svelte
+++ src/lib/components/team/file-card.svelte
@@ -51,7 +51,7 @@
             {/if}
         </div>
         <div class="flex-grow">
-            <p class="text-2xl font-semibold text-gray-900 dark:text-white">
+            <p class="text-2xl font-semibold text-gray-700 dark:text-gray-200">
                 {file_name}
             </p>
             <p class="text-base text-gray-500 dark:text-gray-400">
